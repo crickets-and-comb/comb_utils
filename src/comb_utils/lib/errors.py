3c18f2654b90bb27847399c51b42ce145755919b
Kaleb Coberly <kalebcoberly@gmail.com>
2025-10-18 19:11:40 -0700

    Drop unneeded passes.

--- src/comb_utils/lib/errors.py
+++ src/comb_utils/lib/errors.py
@@ -4,10 +4,6 @@
 class CombUtilsError(Exception):
     """Base class for exceptions in this module."""
 
-    pass
-
 
 class DuplicateKeysDetected(CombUtilsError):
     """Raised when a user enters a query string key twice."""
-
-    pass
