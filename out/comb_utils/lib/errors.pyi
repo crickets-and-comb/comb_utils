c423cbb69ef9844b8cfabb14234845a84d2396b5
Kaleb Coberly <kalebcoberly@gmail.com>
2026-02-13 13:07:35 -0800

    Drop stub files.

--- out/comb_utils/lib/errors.pyi
+++
@@ -1,4 +0,0 @@
-class CombUtilsError(Exception):
-    """Base class for exceptions in this module."""
-class DuplicateKeysDetected(CombUtilsError):
-    """Raised when a user enters a query string key twice."""
